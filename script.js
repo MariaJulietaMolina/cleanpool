2195353a0e3194901b66fac16e95c25b9a6f70ed
Julieta <mariajulietamolina91@gmail.com>
2024-09-19 20:52:14 -0300

    agregado de calculadora y su funcionamiento, terminacion del css general

--- script.js
+++ script.js
@@ -57,3 +57,52 @@ function calcular() {
         document.getElementById("display").value = "Error";
     }
 }
+
+//calculadora de productos
+function calcular() {
+    // entrada de valores
+    const tipoAgua = document.getElementById('tipoAgua').value;
+    const volumen = parseInt(document.getElementById('volumen').value);
+    let productos = '';
+
+    if (volumen > 0) {
+        // calcula los productos por tipo de agua y volumen
+        const tabla = {
+            aguaRed: [
+                {volumen: 5, cloro: 0.5, sssAlum: 0.025, bp65: 0.025, bs77: 0.05},
+                {volumen: 10, cloro: 0.5, sssAlum: 0.075, bp65: 0.075, bs77: 0.100},
+                {volumen: 20, cloro: 1, sssAlum: 0.0100, bp65: 0.150, bs77: 0.200},
+                // seguir agregando valores con la tabla
+            ],
+            aguaPozo1: [
+                {volumen: 5, cloro: 0.5, sssAlum: 0.05, bp65: 0.05, bs77: 0.05},
+                {volumen: 10, cloro: 0.5, sssAlum: 0.1, bp65: 0.15, bs77: 0.1},
+                // seguir agregando valores con la tabla
+            ],
+            aguaPozo2: [
+                {volumen: 5, cloro: 0.5, sssAlum: 0.075, bp65: 0.075, bs77: 0.05},
+                {volumen: 10, cloro: 0.5, sssAlum: 0.15, bp65: 0.15, bs77: 0.1},
+                // seguir agregando valores, ya entendiste
+            ]
+        };
+
+        // Seleccionar resultado según el tipo de agua
+        let data = tabla[tipoAgua].find(item => item.volumen === volumen);
+
+        if (data) {
+            productos = `
+                <strong>Cloro:</strong> ${data.cloro} Lts<br>
+                <strong>SSS Alum:</strong> ${data.sssAlum} Lts<br>
+                <strong>BP65:</strong> ${data.bp65} Lts<br>
+                <strong>BS77:</strong> ${data.bs77} Lts
+            `;
+        } else {
+            productos = 'No se encontraron datos para el volumen especificado.';
+        }
+    } else {
+        productos = 'Por favor, ingresa un volumen válido.';
+    }
+
+    // Mostrar resultado
+    document.getElementById('resultado').innerHTML = productos;
+}
